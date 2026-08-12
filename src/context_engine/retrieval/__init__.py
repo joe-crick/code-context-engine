89bb14629bd42f289cd063d8dfc94cfbb1e9bb25
Joe Crick <j.cick@mailfence.com>
2026-08-12 14:09:05 +0200

    feat: add retrieval fusion helper

---
+++ src/context_engine/retrieval/__init__.py
@@ -0,0 +1,11 @@
+from context_engine.retrieval.fusion import (
+    FusedRetrievalContext,
+    RetrievalFusionInput,
+    fuse_retrieval_context,
+)
+
+__all__ = [
+    "FusedRetrievalContext",
+    "RetrievalFusionInput",
+    "fuse_retrieval_context",
+]

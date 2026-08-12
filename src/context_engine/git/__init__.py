475460ed71cef4046f0c4bbe4c89c89d18c4c0b1
Joe Crick <j.cick@mailfence.com>
2026-08-12 14:00:54 +0200

    feat: add repository storage layout

--- src/context_engine/git/__init__.py
+++ src/context_engine/git/__init__.py
@@ -2,10 +2,13 @@
 
 from context_engine.git.diff import GitWorktreeDiff, get_worktree_diff
 from context_engine.git.repository import GitRepositoryContext, resolve_git_repository_context
+from context_engine.git.storage import RepositoryStorageLayout, repository_storage_layout
 
 __all__ = [
     "GitRepositoryContext",
     "GitWorktreeDiff",
+    "RepositoryStorageLayout",
     "get_worktree_diff",
+    "repository_storage_layout",
     "resolve_git_repository_context",
 ]
